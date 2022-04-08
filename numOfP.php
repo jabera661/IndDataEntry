5a1598a748dcc4b4c51759b10714848eb41396ba
jabera661 <85290809+jabera661@users.noreply.github.com>
2022-04-08 20:36:00 +0300

    Update numOfP.php

--- numOfP.php
+++ numOfP.php
@@ -4,12 +4,14 @@
     </head>
 
     <body>
-
+        <h1 style="text-align: center;">تسجيل بيانات الأفراد</h1>
+        <center>
         <form action="formGen.php" method="POST">
-            <label for="num">عدد الأفراد المراد إدخالهم</label>
-            <input type="number" id="num" name="num">
+            <label for="num">عدد الأفراد المراد إدخالهم</label> <br>
+            <input type="number" id="num" name="num"> <br><br>
             <button type="submit" value="Submit">إرسال</button>
         </form>
+        <center>
 
     </body>
 
